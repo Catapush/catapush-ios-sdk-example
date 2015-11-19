e6efd1ea24a783af5b8c2cce18c31c45be2c4282
Alessandro Chiarotto <chiarotto.alessandro@gmail.com>
2015-11-19 16:32:13 +0100

    Collection inset update

--- catapush-ios-sdk-example/Constants.h
+++ catapush-ios-sdk-example/Constants.h
@@ -11,7 +11,7 @@
 
 #define TIMESTAMP_TEXT_HEIGHT 20
 #define TEXT_CONTAINER_INSET 10
-#define COLLECTION_INSET 0
+#define COLLECTION_INSET 5
 
 #define RGB(r, g, b) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1]
 #define RGBA(r, g, b, a) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:(a)]
